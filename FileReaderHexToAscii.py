a8c09a54fe1e5d3562f7ee0d9401c2d8150af3be
waffy1901 <80718213+waffy1901@users.noreply.github.com>
2022-07-13 12:22:29 -0400

    Update FileReaderHexToAscii.py

--- FileReaderHexToAscii.py
+++ FileReaderHexToAscii.py
@@ -1,3 +1,8 @@
+"""
+Reads hundreds of DCW hexadecimal inputs from a text file and converts them to their ASCII equivalents.
+Produces the output in both the console and a text file (creates a new one).
+"""
+
 import sys
 file = open('Hex.txt')
 for i in file.readlines():
@@ -19,6 +24,18 @@ for i in new:
     
 sys.stdout.close()
 
+"""
+Console Output:
+ì&Ñs¯Ñsv&ÞþïÞ®p%®p/Þ	®®v®0Þþï)»¾®w
+®®(
+®®®Z®
+®®E®8 z} 0
+0(®®pÞÊÈÈÈÈÈÈÈÈÈÈÈÈÈÈÈÈÈÈÈÈ­À 1ÿ
 
-
-
+Text File Output:
+ì  &Ñs ¯Ñs v &Þ þ  ïÞ ®p% ®p/ Þ 	® ®v  ® 0 Þ þ  ï)   »¾  ®w
+ ®  ®( 
+®  ® ®  Z®  
+®  ®E ®8  z }     0 
+ 0 ( ® ®p  Þ   ÊÈÈÈÈÈÈÈÈÈÈÈÈÈÈÈÈÈÈÈÈ­À   1ÿ
+ """
