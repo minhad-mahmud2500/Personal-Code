b0add2980c325da2dfab41047ee152830498c2c6
waffy1901 <80718213+waffy1901@users.noreply.github.com>
2022-07-13 11:35:45 -0400

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,10 @@
+"""
+Script that prints numbers and word(s) between 1 and 100 (both inclusive).
+If the number is a multiple of 3, the word "Fizz" is printed.
+If the number is a multiple of 5, the word "Buzz" is printed.
+If the number is a multiple of both 3 and 5, the words "Fizz Buzz" will be printed.
+Otherwise, strictly the number will be printed.
+"""
 for i in range(1, 101):
     if i % 15 == 0:
         print('Fizz Buzz')
@@ -7,3 +14,107 @@ for i in range(1, 101):
         print('Buzz')
     else:
         print(i)
+
+"""
+Output:
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+Fizz Buzz
+16
+17
+Fizz
+19
+Buzz
+Fizz
+22
+23
+Fizz
+Buzz
+26
+Fizz
+28
+29
+Fizz Buzz
+31
+32
+Fizz
+34
+Buzz
+Fizz
+37
+38
+Fizz
+Buzz
+41
+Fizz
+43
+44
+Fizz Buzz
+46
+47
+Fizz
+49
+Buzz
+Fizz
+52
+53
+Fizz
+Buzz
+56
+Fizz
+58
+59
+Fizz Buzz
+61
+62
+Fizz
+64
+Buzz
+Fizz
+67
+68
+Fizz
+Buzz
+71
+Fizz
+73
+74
+Fizz Buzz
+76
+77
+Fizz
+79
+Buzz
+Fizz
+82
+83
+Fizz
+Buzz
+86
+Fizz
+88
+89
+Fizz Buzz
+91
+92
+Fizz
+94
+Buzz
+Fizz
+97
+98
+Fizz
+Buzz
+"""
