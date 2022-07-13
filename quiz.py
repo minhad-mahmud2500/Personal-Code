63fc2b0eb500bf4e6a32cf9fa425d27327f58856
waffy1901 <80718213+waffy1901@users.noreply.github.com>
2022-07-13 11:48:43 -0400

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,8 +1,16 @@
+"""
+Asks the user if they want to partake in my quiz.
+If they answer Y/y, they will proceed to my quiz. Otherwise, the program will terminate.
+My quiz contains 6 questions and is multiple choice.
+For each question, the user will be informed whether their answer was correct or not.
+Once the user answers all 6 questions, a corresponding grade will be printed (rounded to 2 decimal places).
+"""
+
 print("Welcome to my quiz!")
 
-partake = input("\nDo you want to partake in this quiz? (yes/no) ")
+partake = input("\nDo you want to partake in this quiz - Yes (Y/y) or No (N/n)? ")
 
-if partake.lower() != "yes":
+if partake.lower() != "y":
     quit()
 else:
     print("\nAlright, let's get started!")
@@ -81,4 +89,71 @@ else:
 
 grade = (points / 6) * 100
 print("\nYou got", points, "out of 6 questions correct.")
-print("\nTherefore, you got a " + "%.2f"%grade + "% on this quiz.")
+print("\nYour corresponding grade: " + "%.2f"%grade + "%")
+
+"""
+Sample Output:
+Welcome to my quiz!
+
+Do you want to partake in this quiz - Yes (Y/y) or No (N/n)? Y
+
+Alright, let's get started!
+
+Please choose a letter for each response (a, b, c, or d).
+
+The NFL is comprised of how many teams? 
+a. 25 
+b. 32 
+c. 28 
+d. 40 
+a
+
+Sorry, that is incorrect.
+
+At Home Depot, what does ASM stand for? 
+a. Assistant Store Manager 
+b. Assistant Specialist Manager 
+c. Annual Stockholders Meeting 
+d. Applications Sales Manager 
+a
+
+Well done, that is correct.
+
+Which Calculus course introduces the z variable? 
+a. Calculus II 
+b. Calculus IV 
+c. Calculus III 
+d. Calculus I 
+c
+
+Well done, that is correct.
+
+What is the largest country in the world (in terms of land)? 
+a. USA 
+b. China 
+c. India 
+d. Russia 
+d
+
+Well done, that is correct.
+
+Are speed and velocity both vector quantities? 
+a. no 
+b. yes 
+a
+
+Well done, that is correct.
+
+What is sin(30) equivalent to? 
+a. square root of 3 / 2 
+b. square root of 2 / 2 
+c. 0 
+d. 1/2 
+d
+
+Well done, that is correct.
+
+You got 5 out of 6 questions correct.
+
+Your corresponding grade: 83.33%
+"""
