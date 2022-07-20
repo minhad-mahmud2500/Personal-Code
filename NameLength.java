37ee0159c3a3386e71275ee38b03cf21ea8b3b54
waffy1901 <80718213+waffy1901@users.noreply.github.com>
2022-07-20 12:25:44 -0400

    Add files via upload

---
+++ NameLength.java
@@ -0,0 +1,22 @@
+/* 
+ * Can take in names of any length and will return the correct amount of letters in the given name.
+ * Accounts for white spaces to produce the correct length.
+ */
+
+import java.util.Scanner;
+
+public class NameLength {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Please enter your full name: ");
+		String name = scan.nextLine();
+		
+		System.out.println("Your name has a length of " + name.replace(" ", "").length() + " letters.");
+	}
+}
+
+/*
+ * Sample Output:
+ * Please enter your full name: Olamide Otunjele Bagune Ratibu
+ * Your name has a length of 27 letters.
+*/
